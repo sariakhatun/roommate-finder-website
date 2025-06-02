1aba2d414c718a26356bdce661e54a329fcf10d1
Saria Khatun <sariajuiit@gmail.com>
2025-06-02 17:51:54 +0600

    rlottie-react and react-awesome-reveal used in this section

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -31,7 +31,7 @@ let navigate =useNavigate();
 
 
   return (
-    <div className="navbar bg-[#fcfbe8] shadow-sm px-6">
+    <div className="navbar bg-[#fcfbe8] shadow-sm px-6 w-11/12 mx-auto">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
